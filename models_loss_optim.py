db0bf36dcc860597b5e3588856875bbbf8715fe4
Marcel Mann <you@example.com>
2025-07-30 22:46:44 +0200

    commit

--- models_loss_optim.py
+++ models_loss_optim.py
@@ -25,8 +25,9 @@ class MoonModelV2(nn.Module):
         super().__init__()
         self.layer_stack = nn.Sequential(
             nn.Linear(in_features=input_features, out_features=hidden_layer),
+            nn.BatchNorm1d(hidden_layer),
             nn.Tanh(),
-            nn.Linear(in_features=input_features, out_features=output_features)
+            nn.Linear(in_features=hidden_layer, out_features=output_features)
         )
         
     def forward(self, x):
@@ -38,6 +39,7 @@ class MoonModelV3(nn.Module):
         super().__init__()
         self.layer_stack = nn.Sequential(
             nn.Linear(in_features=input_features, out_features=hidden_layer),
+            nn.BatchNorm1d(hidden_layer),
             nn.GELU(),
             nn.Linear(in_features=hidden_layer, out_features=output_features)
         )
@@ -47,15 +49,15 @@ class MoonModelV3(nn.Module):
     
 
 moon_model_V1_1 = MoonModelV1(2, 1, 16).to(device)
-moon_model_V1_2 = MoonModelV1(2, 1, 64).to(device)
+moon_model_V1_2 = MoonModelV1(2, 1, 128).to(device)
 
 
 moon_model_V2_1 = MoonModelV2(2, 1, 16).to(device)
-moon_model_V2_2 = MoonModelV2(2, 1, 64).to(device)
+moon_model_V2_2 = MoonModelV2(2, 1, 128).to(device)
 
 
 moon_model_V3_1 = MoonModelV3(2, 1, 16).to(device)
-moon_model_V3_2 = MoonModelV3(2, 1, 64).to(device)
+moon_model_V3_2 = MoonModelV3(2, 1, 128).to(device)
 
 
 # ------------------------------------------------
